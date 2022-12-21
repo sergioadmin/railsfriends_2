3351cbad30e2b2892e1157a958476b84c163a6d1
alex <sergioadmin.rus@gmail.com>
2022-12-21 15:39:12 +0200

    Init commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
+  devise_for :users
+  resources :friends
+  get 'home/about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "home#index"
+  root "friends#index"
+end
